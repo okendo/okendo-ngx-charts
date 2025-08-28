4f03b7c766c8297e1c23cc60c2b7b86e573dab12
Tom Fulcher <tlgfulcher@gmail.com>
2025-08-28 17:33:42 +1000

    Update bar-vertical.component.spec.ts

--- projects/swimlane/ngx-charts/src/lib/bar-chart/bar-vertical.component.spec.ts
+++ projects/swimlane/ngx-charts/src/lib/bar-chart/bar-vertical.component.spec.ts
@@ -229,7 +229,7 @@ describe('<ngx-charts-bar-vertical>', () => {
 });
 
 describe('bar-max-width', () => {
-  it('should render correct cell size, with zero padding, but fixed width', async(() => {
+  it('should render correct cell size, with zero padding, but fixed width', (() => {
     TestBed.overrideComponent(TestComponent, {
       set: {
         template: `
